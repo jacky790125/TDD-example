4e9ae2a056b47ea570f0a6b68182a242b3860316
Hsieh Chang Wei <jacky790125@gmail.com>
2020-01-07 17:44:20 +0800

    Add Calculate Feature

--- src/PaymentList.php
+++ src/PaymentList.php
@@ -2,8 +2,6 @@
 
 namespace Src;
 
-use Salary;
-
 class PaymentList
 {
     public $list = array();
@@ -11,5 +9,14 @@ class PaymentList
     public function insert($salary)
     {
         $this->list[] = $salary;
+    }
+
+    public function calculateSum()
+    {
+        $sum = 0;
+        foreach ($this->list as $temp) {
+            $sum = $sum + $temp->value;
+        }
+        return $sum;
     }
 }
