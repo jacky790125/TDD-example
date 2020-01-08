84410f71fc0dd03f99a8af7e303cd5413c1e0a9d
Hsieh Chang Wei <jacky790125@gmail.com>
2020-01-08 16:18:17 +0800

    Use Mockery

--- tests/PaydayTest.php
+++ tests/PaydayTest.php
@@ -11,4 +11,17 @@ class PaydayTest extends TestCase
         $result = $payday->pay();
         $this->assertSame('paid', $result[0]);
     }
+
+    public function testPaydayByMock()
+    {
+        $payday = Mockery::mock(Payday::class);
+        $payday->shouldReceive('pay')
+            ->once()
+            ->andReturn(['paid','first']);
+        
+        $result = $payday->pay();
+        $this->assertSame('paid', $result[0]);
+
+        Mockery::close();
+    }
 }
