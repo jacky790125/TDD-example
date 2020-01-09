0348dee96947e63321eab3cf4f62d8ebd4b049de
Hsieh Chang Wei <jacky790125@gmail.com>
2020-01-09 18:02:17 +0800

    fix: disable telescope. clear cache and config

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -5,6 +5,7 @@ namespace Tests\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
+use App\User;
 
 class LoginTest extends DuskTestCase
 {
@@ -17,7 +18,24 @@ class LoginTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-                    ->assertSee('Laravel');
+                ->assertSee('Laravel');
+        });
+    }
+
+    public function testLogin()
+    {
+        User::truncate();
+
+        $user = factory(User::class)->create([
+            'email' => 'taylor@laravel.com',
+        ]);
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                ->type('email', $user->email)
+                ->type('password', 'password')
+                ->press('Login')
+                ->assertPathIs('/home');
         });
     }
 }
