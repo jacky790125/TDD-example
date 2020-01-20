41eac2da10e6ffec7af241354e07d83f1f86d336
Hsieh Chang Wei <jacky790125@gmail.com>
2020-01-20 13:25:08 +0800

    fix: view bug

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -14,12 +14,22 @@
 
                         <div class="card-body">
                             {{ $post->content }}
+                            <div class="card">
+                                <div class="card-header">Comments:</div>
+
+                                @foreach($post->comments as $comment)
+                                    <div class="card-body">
+                                        {{ $comment->user->name.': '.$comment->comment }}
+                                    </div>
+                                @endforeach
+                            </div>
                         </div>
+
                         <form method="post" action="/posts/comment">
                             @csrf
                             Leave a Comment:
                             <input type="text" size="30" name="comment">
-                            <input type="hidden" name="user_id" value={{ $post->user->id }}>
+                            <input type="hidden" name="post_id" value={{ $post->id }}>
                             <input type="submit" value="Send">
                         </form>
                     </div>
